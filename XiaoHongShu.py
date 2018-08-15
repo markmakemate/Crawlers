cfd4019767b3a9b6bbad8b38c522e25962e0c692
markmakemate <949109582@qq.com>
2018-08-15 17:58:54 +0800

    Modified

--- XiaoHongShu.py
+++ XiaoHongShu.py
@@ -1,32 +1,98 @@
 import scrapy
+import nltk #A NLP library, I plan to apply it to analyse articles#
 from scrapy.exporters import JsonItemExporter
-from scrapy.loader import ItemLoader 
-class MyItem(Item):
+from scrapy.loader import ItemLoader
+#A crawler which can scrape fixed data from 小红書, implementing with the framework Scrapy#
+
+#Loader of Kol's profile#
+class KOL_Profile(Item):
     name=scrapy.Field()
     follower=scrapy.Field() 
-class XiaoHongShu(scrapy.Spider):
-    name="Xiaohongshu"
-    def __init__(self,category=None,*args,*kwargs):
-        super(XiaoHongShu,self).__init__(*args,*kwargs)
-        self.allowed_domain=['xianghongshu.com']
-        self.start_url='http://www.xiaohongshu.com/explore/?tab=cosmetics'
-        self.User=ItemLoader(item=MyItem(),response=response)
-    def parse(self,response):
+    like&collection=scrapy.Field()
+    tag=scrapy.Field()
+    url=scrapy.Field()
+
+#Loader of kol's articles#
+class KOL_Article(Item):
+    KOL_name=scrapy.Field()
+    tags=scrapy.Field()
+    content=scrapy.Field()
+    like_number=scrapy.Field()
+    star_number=scrapy.Field()
+    comment=scrapy.Field()
+
+#Profile data processing#
+class Profile:
+    def parse_kol_profile(self,response):
+        Profile=ItemLoader(item=KOL_Profile(),response=response)
         for url in response.xpath('//a[@class="note-href"]/@href').extract():
-            yield scrapy.Request(url,callback=self.parse_items)
-        return self.User.load_item()
-    def parse_items(self,response):
-        self.User.add_xpath('name','//div[@class="right-card"]/a//h6[@class="name"/span/text()]')
-        self.User.add_xpath('follower','//div[@class="right-card"]/a//h6[@class="name"/span/text()]')
-    def export_items(self,info):
-        f=open('/home/user_info/user_info.json','wb')
+            yield scrapy.Request(url,callback=self.kol_profile_parser(Profile))
+        return Profile
+
+    #Interface that the class "XiaoHongShu" will implement#
+    def kol_profile_parser(self,response,itemloader):
+        Profile=itemloader
+        Profile.add_xpath('name','//span[@class="name-detail"]/text()')
+        Profile.add_xpath('follower','//span[@class="fans"]/text()')
+        Profile.add_xpath('like&collection','//span[@class="collect"]/text()')
+        Profile.add_xpath('tag','//p[@class="brief"]/text()')
+        Profile.add_xpath('url','//a[@class=author-info]/@href')
+
+#Article's data processing#
+class Article:
+    def parse_kol_article(self,kol_url):
+        yield scrapy.Request(kol_url,callback=self.storeArticle)
+
+    def storeAriticle(self,response):
+        itemloader=ItemLoader(item=KOL_Article(),response=response)
+        for url in response.xpath('//a[@class="note-href"]/@href').extract():
+            yield scrapy.Request(url,callback=self.storage(itemloader))
+        return itemloader.load_item()
+
+    #Storage progression#
+    def storage(self,response,itemloader):
+        for name in response.xpath('//span[@class="name-detail"]/text()').extract():
+            itemloader.add_value('KOL_name',name)
+        for note in response.xpath('//div[@class="related-tags"]//a/text()').extract():
+            itemloader.add_value('tags',note)
+        for Content in response.xpath('//p[@class="content"]//p/text()').extract():
+            itemloader.add_value('content',Content)
+        for read_number in response.xpath('//'):
+            itemloader.add_value('read_number',read_number)
+        for like_number in response.xpath('//span[@class="like"]/span/text()').extract():
+            itemloader.add_value('like_number',read_number)
+        for comment in response.xpath(''):
+            itemloader.add_value('comment',comment)
+        for star in response.xpath('//span[@class="star"]/span/text()').extract():
+            itemloader.add_value('star_number',star)
+        getContent(itemloader)
+    
+    #join content list#
+    def getContent(self,itemloader):
+        text='\n'.join(itemloader['content'])
+        itemloader.replace_value('content',text)
+
+#Users can apply the interfaces and configure parameters in this class to crawl the data they want#
+#Transaction the data that have been scraped to json file#
+class XiaoHongShu(scrapy.Spider,Article,Profile):
+    name="XiaoHongShu"
+    allowed_domain=['.xiaohongshu.com']
+    def __init__(self,tab,catagory=None,*arg,*kwarg):
+        super(XiaoHongShu,scrapy.Spider).__init__(*arg,*kwarg)
+        self.start_url='%s%s%s'%('http://www.xiaohongshu.com/explore','?tab=',tab)
+    def parse(self,response):
+        yield super(XiaoHongShu,Profile).parse_kol_profile(response)
+    def parse_article(self,kol_url):
+        yield super(XiaoHongShu,Article).parse_kol_article(kol_url)
+    def export_items(self,info,path,file_name):
+        f=open('/'.join[path,file_name],'wb')
         export=JsonItemExporter(f)
         export.start_exporting()
         export.export_item(info)
         return export
-XiaoHongShu XHS
-XHS.export_items(XHS.parse())
-XHS.export_items.finish_exporting()
+
+#Modeling Modula#
+class Modeling:
 
 
 
